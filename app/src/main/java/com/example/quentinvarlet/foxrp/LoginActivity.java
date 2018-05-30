153c4b64ea00d8ecd30a4c80a47d5e4040cd33ff
Quentin <quentin.varlet@gmail.com>
2018-05-30 14:08:52 +0200

    CONNEXION FONCTIONNEL SAUVEGARDE DE L'EMAIL ET DU MOT DE PASSE LORS DE LA FERMETURE DE L'APPLI

--- app/src/main/java/com/example/quentinvarlet/foxrp/LoginActivity.java
+++ app/src/main/java/com/example/quentinvarlet/foxrp/LoginActivity.java
@@ -1,29 +1,63 @@
 package com.example.quentinvarlet.foxrp;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements TextWatcher, CompoundButton.OnCheckedChangeListener {
 
     EditText email, password;
     Button btnLogin;
     AlertDialog.Builder builder;
+    private CheckBox rem_userpass;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    private static final String PREF_NAME = "prefs";
+    private static final String KEY_REMEMBER = "remember";
+    private static final String KEY_USERNAME = "email";
+    private static final String KEY_PASS = "password";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Partie Connexion automatique
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
+        rem_userpass = (CheckBox)findViewById(R.id.checkBox);
 
+        if(sharedPreferences.getBoolean(KEY_REMEMBER, false))
+            rem_userpass.setChecked(true);
+        else
+            rem_userpass.setChecked(false);
+
+        email.setText(sharedPreferences.getString(KEY_USERNAME,""));
+        password.setText(sharedPreferences.getString(KEY_PASS, ""));
+
+        email.addTextChangedListener(this);
+        password.addTextChangedListener(this);
+        rem_userpass.setOnCheckedChangeListener(this);
+        //rem_userpass.setOnCheckedChangeListener(this);
+
+        //Partie Connexion
         btnLogin = (Button) findViewById(R.id.login_button);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,4 +81,42 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        managePrefs();
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        managePrefs();
+    }
+
+    private void managePrefs(){
+        if(rem_userpass.isChecked()){
+            editor.putString(KEY_USERNAME, email.getText().toString().trim());
+            editor.putString(KEY_PASS, password.getText().toString().trim());
+            editor.putBoolean(KEY_REMEMBER, true);
+            editor.apply();
+        }else{
+            editor.putBoolean(KEY_REMEMBER, false);
+            editor.remove(KEY_PASS);//editor.putString(KEY_PASS,"");
+            editor.remove(KEY_USERNAME);//editor.putString(KEY_USERNAME, "");
+            editor.apply();
+        }
+    }
+
+
+
 }
